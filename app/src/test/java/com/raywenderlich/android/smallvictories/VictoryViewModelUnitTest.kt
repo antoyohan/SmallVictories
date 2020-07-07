e01f215770b843a05ad349e354761b5bdc7c8bec
Anto Yohan <antoyohan@gmail.com>
2020-07-07 17:57:17 -0400

    pass tests

--- app/src/test/java/com/raywenderlich/android/smallvictories/VictoryViewModelUnitTest.kt
+++ app/src/test/java/com/raywenderlich/android/smallvictories/VictoryViewModelUnitTest.kt
@@ -11,6 +11,6 @@ class VictoryViewModelUnitTest {
         var viewModel = VictoryViewModel()
         val res = viewModel.addNumbers(10, 15)
 
-        assertEquals(15, res)
+        assertEquals(25, res)
     }
 }
